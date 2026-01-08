04f1f957e01a369a592f529763c91c1a1e4ee38d
elliecdev <elliec.dev@gmail.com>
2026-01-08 16:04:37 -0500

    setup initial templating and basic home page

--- steps/views.py
+++ steps/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
 
+challenges = [
+    {
+        'start': 'November 1, 2025',
+        'end': 'December 31, 2025',
+        'active': True,
+        'name': 'Winter 2025 Challenge'
+    },
+    {
+        'start': 'August 1, 2025',
+        'end': 'September 31, 2025',
+        'active': False,
+        'name': 'Fall 2025 Challenge'
+    },
+    {
+        'start': 'June 1, 2025',
+        'end': 'July 31, 2025',
+        'active': False,
+        'name': 'Summer 2025 Challenge'
+    },
+    {
+        'start': 'March 1, 2025',
+        'end': 'April 31, 2025',
+        'active': False,
+        'name': 'Spring 2025 Challenge'
+    }
+]
+
 
 def home(request):
-    return render(request, 'steps/home.html')
+    context = {
+        'current_challenge': challenges[0],
+        'challenges': challenges,
+    }
+    return render(request, 'steps/home.html', context)
